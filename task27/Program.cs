5022c0e529c2999de8dd4663c1fc3ac2fcf4434f
Adalberto777 <albert.tliashev@gmail.com>
2022-08-30 18:37:29 +0500

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// принимаем на вход число и выдаём сумму цифр в числе.
+
+int SumDigit(int number) 
+{
+    int sum = 0;
+    int digit = 0;
+    
+        while(number > 0)
+    {
+        digit = number % 10; // отрезаем по одной крайней цифре от остатка первоначального числа за каждый цикл
+        sum = sum + digit;   // добавляем отрезанное число в сумматор
+        number = number / 10;// обрезаем на одну крайнюю цифру первоначальное число за каждый цикл
+    }
+    return sum;
+}
+
+Console.WriteLine("Enter digit");
+int number = int.Parse(Console.ReadLine());
+
+int resultP = SumDigit(number);
+Console.WriteLine($"Сумма цифр числа {number} = {resultP}");
