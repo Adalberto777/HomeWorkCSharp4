7d0c96734199ef913a72a3403297c8e75e457a42
Adalberto777 <albert.tliashev@gmail.com>
2022-08-30 16:16:18 +0500

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,23 @@
+﻿// принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+double PowLite(double a, double b) 
+{
+    double resultF = 1;
+    for(int i = 1; i <= b; i++)
+    {
+        resultF = resultF * a;
+    }
+    return resultF;
+}
+
+Console.WriteLine("Enter а integer number ");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a degree ");
+double b = double.Parse(Console.ReadLine());
+
+double resultP = PowLite(a, b);
+Console.WriteLine($"{a} ^ {b} = {resultP}");
+
+// double resultP2 = Math.Pow(a, b);
+// Console.WriteLine(resultP2);
